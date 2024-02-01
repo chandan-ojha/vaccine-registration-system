95e8dfbfa93ca20799fa0636e434645b2cd51a4a
Chandan Ojha <chandanojha776@gmail.com>
2024-02-01 14:33:25 +0600

    updated

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <title>{{ config('app.name') }}</title>
+    <title>Vaccination Program</title>
 
     <style>
         [x-cloak] {
